c7ef1625f2f73cce3c08080fb93745a8e4266776
Mark Cooper <markchristophercooper@gmail.com>
2017-11-09 10:14:37 -0800

    Add google auth

--- frontend/plugin_init.rb
+++ frontend/plugin_init.rb
@@ -3,19 +3,25 @@ oauth_definition = AppConfig[:authentication_sources].find { |as|
 }
 raise "OmniAuth plugin enabled but no definition provided =(" unless oauth_definition
 
+# GOOGLE STRATEGY FOR TESTING [DO NOT USE IN PROD UNLESS YOU MEAN TO]
+def google_oauth_enabled?
+  ENV.has_key?('GOOGLE_CLIENT_ID') and ENV.has_key?('GOOGLE_CLIENT_SECRET')
+end
+
 AppConfig[:oauth_provider] = oauth_definition[:provider]
 ArchivesSpace::Application.extend_aspace_routes(File.join(File.dirname(__FILE__), "routes.rb"))
 
-OAUTH_STRATEGIES = [
-  'cas',
-  'saml',
-].freeze
-
 require 'omniauth'
-if OAUTH_STRATEGIES.include? oauth_definition[:provider]
-  require "omniauth/#{oauth_definition[:provider]}"
-end
+require 'omniauth-google-oauth2' if google_oauth_enabled?
 
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider oauth_definition[:provider], oauth_definition[:config]
+  if google_oauth_enabled?
+    provider :google_oauth2,
+      ENV['GOOGLE_CLIENT_ID'],
+      ENV['GOOGLE_CLIENT_SECRET'],
+      access_type: 'online',
+      prompt: ''
+  else
+    provider oauth_definition[:provider], oauth_definition[:config]
+  end
 end
