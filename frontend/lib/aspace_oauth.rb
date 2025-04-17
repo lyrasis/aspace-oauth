c2538745d5f10aa569d45d112a915c7762140071
dl-maura <maura_carbone@harvard.edu>
2025-04-17 10:47:35 -0400

    Update aspace_oauth.rb

--- frontend/lib/aspace_oauth.rb
+++ frontend/lib/aspace_oauth.rb
@@ -43,14 +43,15 @@ module AspaceOauth
     AppConfig[:oauth_definitions].find { |oauth| oauth[:provider] == strategy }
   end
 
+
   def self.saml_logout_url
     config = get_oauth_config_for("saml")
     return unless config
-
+    
     host = config[:config][:idp_slo_service_url]
     
-    if config[:config][:idp_slo_service_url]
-      config[:config][:idp_slo_service_url].to_s
+    if host
+      host.to_s
     else
       build_url(
         AppConfig[:frontend_proxy_url],
